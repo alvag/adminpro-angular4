510b1a81c1ec941d9dc540e03886c6bb6dafeb4a
Max Alva <alva85@gmail.com>
2018-01-23 21:51:26 -0500

    servicio login y opcion de recordar email

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
+import { UsuarioService } from "./../services/service.index";
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { NgForm } from "@angular/forms";
+import { Usuario } from "../models/usuario.model";
 
 declare function init_plugins();
 
@@ -9,16 +12,32 @@ declare function init_plugins();
     styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
+    recuerdame: boolean = false;
+    email: string = localStorage.getItem("email");
 
-    constructor(private router: Router) {
-    }
+    constructor(
+        private router: Router,
+        private usuarioService: UsuarioService
+    ) {}
 
     ngOnInit() {
         init_plugins();
+
+        if (this.email.length > 0) {
+            this.recuerdame = true;
+        }
     }
 
-    ingresar() {
-        this.router.navigate(["/dashboard"]);
+    ingresar(form: NgForm) {
+        if (form.valid) {
+            let usuario = new Usuario(
+                null,
+                form.value.email,
+                form.value.password
+            );
+            this.usuarioService
+                .login(usuario, form.value.recuerdame)
+                .subscribe(response => this.router.navigate(["/dashboard"]));
+        }
     }
-
 }
--- src/app/services/usuario/usuario.service.ts
+++ src/app/services/usuario/usuario.service.ts
@@ -15,4 +15,20 @@ export class UsuarioService {
             return response.usuario;
         });
     }
+
+    login(usuario: Usuario, recordar: boolean = false) {
+        if (recordar) {
+            localStorage.setItem("email", usuario.email);
+        } else {
+            localStorage.removeItem("email");
+        }
+
+        let url = environment.API_URL + "login";
+        return this.http.post(url, usuario).map((response: any) => {
+            localStorage.setItem("id", response.usuario._id);
+            localStorage.setItem("token", response.token);
+            localStorage.setItem("usuario", JSON.stringify(response.usuario));
+            return true;
+        });
+    }
 }
